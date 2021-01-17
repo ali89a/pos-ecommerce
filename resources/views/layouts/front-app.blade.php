2be74765c3d6a0eb6c154bbf8e1d60bbd120f722
Atiqur Rahaman <toatikbd@gmail.com>
2021-01-17 18:03:02 +0600

    update front app page

--- resources/views/layouts/front-app.blade.php
+++ resources/views/layouts/front-app.blade.php
@@ -51,45 +51,9 @@
                 <div class="container">
                     <div class="row">
                         <div class="header-top-left col-lg-6 col-md-4 col-sm-6 col-xs-7">
-                            <ul class="top-link list-inline lang-curr">
-                                <li class="currency">
-                                    <div class="btn-group currencies-block">
-                                        <form action="#" method="post" enctype="multipart/form-data" id="currency">
-                                            <a class="btn btn-link dropdown-toggle" data-toggle="dropdown">
-                                                <span class="icon icon-credit "></span> $ US Dollar <span
-                                                    class="fa fa-angle-down"></span>
-                                            </a>
-                                            <ul class="dropdown-menu btn-xs">
-                                                <li> <a href="#">(€)&nbsp;Euro</a></li>
-                                                <li> <a href="#">(£)&nbsp;Pounds </a></li>
-                                                <li> <a href="#">($)&nbsp;US Dollar </a></li>
-                                            </ul>
-                                        </form>
-                                    </div>
-                                </li>
-                                <li class="language">
-                                    <div class="btn-group languages-block ">
-                                        <form action="#" method="post" enctype="multipart/form-data" id="bt-language">
-                                            <a class="btn-link dropdown-toggle" data-toggle="dropdown">
-                                                <img src="{{ asset('front') }}/image/catalog/flags/gb.png" alt="English"
-                                                    title="English">
-                                                <span class="">English</span>
-                                                <span class="fa fa-angle-down"></span>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li>
-                                                    <a href="#"><img class="image_flag"
-                                                            src="{{ asset('front') }}/image/catalog/flags/gb.png"
-                                                            alt="English" title="English" /> English </a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"> <img class="image_flag"
-                                                            src="{{ asset('front') }}/image/catalog/flags/ar.png"
-                                                            alt="Arabic" title="Arabic" /> Arabic </a>
-                                                </li>
-                                            </ul>
-                                        </form>
-                                    </div>
+                            <ul class="top-link list-inline">
+                                <li class="checkout hidden-sm hidden-xs"><a href="#" class="btn-link"
+                                        title="Checkout "><span><i class="fa fa-check-square-o"></i>Checkout </span></a>
                                 </li>
                             </ul>
                         </div>
