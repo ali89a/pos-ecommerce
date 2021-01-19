13839db2f6c837292dc2f7d5018f58000da6d4df
Atiqur Rahaman <toatikbd@gmail.com>
2021-01-19 17:22:51 +0600

    update admin navbar and sidebar area

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -5,7 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
+            <a href="{{route('home')}}" class="nav-link">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="#" class="nav-link">Contact</a>
@@ -50,7 +50,7 @@
             <a class="d-block" data-toggle="dropdown" href="#">
                 <div class="user-panel pr-3 d-flex">
                     <div class="info">
-                        <span class="d-block">{{ \Auth::user()->name  ?? '' }}</span>
+                        <span class="d-block">Account {{ \Auth::user()->name  ?? '' }}</span>
                     </div>
                 </div>
             </a>
