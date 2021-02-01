2bd33b00c673b728f3e5f8c87a0c73f95f80ad2e
Atiqur Rahaman <toatikbd@gmail.com>
2021-02-01 17:50:06 +0600

    start pos page design

--- resources/views/admin/pos/index.blade.php
+++ resources/views/admin/pos/index.blade.php
@@ -12,7 +12,28 @@ POS
 @endsection
 
 @section('content')
----------------
+<!-- Content Header (Page header) -->
+
+<section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-1">
+
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-6">Location:   Awesome Shop  02/01/2021 03:00</div>
+                <div class="col-6">Location:   Awesome Shop  02/01/2021 03:00</div>
+            </div>
+        </div>
+    </section>
+    <!-- / .min content  -->
+</section>
 @endsection
 
 @push('script-bottom')
